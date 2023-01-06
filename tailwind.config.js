f4a1bf6ddab3cefdb70b790b8c29609d6549b9c5
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2023-01-05 21:53:54 -0600

    Imported Montserrat and updated tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      letterSpacing: {
+        loose: ".15rem",
+        looser: ".2rem",
+        loosest: ".25rem",
+        expanded: ".5rem",
+      },
+      colors: {
+        primary: {
+          light: "#d8884b",
+          DEFAULT: "#d27731",
+          dark: "#BC6929",
+          darker: "#a75d25",
+          darkest: "#864B1D",
+          bright: "#ee7f2b",
+        },
+        secondary: "#708090",
+      },
+    },
+    fontFamily: {
+      primary: ["Anybody"],
+      secondary: ["Montserrat"],
+    },
   },
   plugins: [],
 };
